bee53098ae5dbc54a97224aee0458b0c69657721
Rohit Sudhakaran <58155944+rsk210@users.noreply.github.com>
2022-10-27 22:31:48 +1100

    Data Extension Key Logger

--- routes/activity.js
+++ routes/activity.js
@@ -31,6 +31,7 @@ exports.execute = async (req, res) => {
   try {
     const id = Uuidv1();
     logger.debug("In the data extension function")
+    logger.debug(process.env.DATA_EXTENSION_EXTERNAL_KEY)
     await SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
       {
         keys: {
