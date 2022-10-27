3c0bc774560155d308e2f74962e8505a10a9143a
Rohit Sudhakaran <58155944+rsk210@users.noreply.github.com>
2022-10-27 21:39:18 +1100

    Logger for DE

--- routes/activity.js
+++ routes/activity.js
@@ -29,7 +29,7 @@ exports.execute = async (req, res) => {
 
   try {
     const id = Uuidv1();
-
+    logger.debug("In the data extension function")
     await SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
       {
         keys: {
